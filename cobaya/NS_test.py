184adde0de5f57a07d8362e4eda3176d1e09ee69
Xyeeee <53278668+Xyeeee@users.noreply.github.com>
2022-03-02 17:40:14 +0000

    changed typing file name into typingg to avoid circular import. ugh

--- cobaya/NS_test.py
+++ cobaya/NS_test.py
@@ -1,13 +1,13 @@
 from cobaya.run import run
 
 run("NS_default.yaml",
-        packages_path="packages",
-        output="runs/NS_default",
-        debug=False,
-        stop_at_error=True,
-        resume=False,
-        force=False,
-        no_mpi=True,
-        test=False,
-        override=None
-        )
+    packages_path="packages",
+    output="runs/NS_default",
+    debug=False,
+    stop_at_error=True,
+    resume=False,
+    force=False,
+    no_mpi=True,
+    test=False,
+    override=None
+    )
